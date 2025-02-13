be64bda89705fdabca9db0e6bf50e7a171de6b5b
Juan Luis Barberia <50841177+jbarberia@users.noreply.github.com>
2025-02-13 17:13:24 -0300

    minusculas para compatibilidad con python 2

--- tests/test_00.py
+++ tests/test_00.py
@@ -1,19 +1,19 @@
 import psse34
 import psspy
-import PlantillaObrasTransba
-from PlantillaObrasTransba import AREA_ATLANTICA, TRAFO_110_110_40, TRAFO_NORDEX_163_57, AERO_NORDEX_163_57
+import plantillaobrastransba
+from plantillaobrastransba import AREA_ATLANTICA, TRAFO_110_110_40, TRAFO_NORDEX_163_57, AERO_NORDEX_163_57
 
 psspy.psseinit()
 
 def test_00():
     psspy.newcase_2()
 
-    COLECTOR = {
-        "r": 0.00399, # PU
-        "x": 0.00430, # PU
-        "b": 0.01669, # PU
-    }
-    PlantillaObrasTransba.crea_PPEE(2871, 2770, "ARMONIA", "VATLEO01", AREA_ATLANTICA, TRAFO_110_110_40, COLECTOR, 18*[TRAFO_NORDEX_163_57], 18*[AERO_NORDEX_163_57])
+COLECTOR = {
+    "r": 0.00399, # PU
+    "x": 0.00430, # PU
+    "b": 0.01669, # PU
+}
+plantillaobrastransba.crea_PPEE(2871, 2770, "ARMONIA", "VATLEO01", AREA_ATLANTICA, TRAFO_110_110_40, COLECTOR, 18*[TRAFO_NORDEX_163_57], 18*[AERO_NORDEX_163_57])
 
     assert psspy.busexs(287131) == 0
     assert psspy.busexs(287132) == 0
